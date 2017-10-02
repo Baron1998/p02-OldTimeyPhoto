fa20f14ca9048360612deadd2a80d3db1024b18e
Baron1998 <abaron3@mail.csuchico.edu.com>
2017-10-02 16:37:13 -0700

    comments version

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Author : Abdullah Baron 
+#include<string>
+#include<vector>
+#include "bitmap.h"
+#include<iostream>
+
+using namespce std;
+int main() 
+{
+//declare veriables (Bitmap) ,(vector <vector <Pixel> > ) , and (Pixel).
+//output "what is the file name?" must be an image with BMP format
+//input the image that the user choose 
+// choose values for r , g , and b let's say 50 
+// when r=50 , g=50 , and b=50 we will git a gray color = 50
+//now we choose these values for every pixel of that image.
+//to change every pixel we need to use a loop(for loop) to make sure that every pixel will change to gray 
+//save the new image.
+  return 0;
+}
